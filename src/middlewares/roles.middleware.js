5f8953435a476cfdb53c541d8ec6722c27d8f015
Jotorresa <jotorresa@unal.edu.co>
2026-06-20 16:07:41 -0500

    Ajustes de Middlewares

--- src/middlewares/roles.middleware.js
+++ src/middlewares/roles.middleware.js
@@ -2,10 +2,15 @@ const verificarRol = (...rolesPermitidos) => {
 
     return (req, res, next) => {
 
-        if (!rolesPermitidos.includes(req.usuario.rol)) {
+        if (!req.usuario) {
+            return res.status(401).json({
+                mensaje: 'Usuario no autenticado'
+            });
+        }
 
+        if (!rolesPermitidos.includes(req.usuario.rol)) {
             return res.status(403).json({
-                mensaje: 'No tienes permisos'
+                mensaje: 'No tienes permisos para realizar esta acción'
             });
         }
 
